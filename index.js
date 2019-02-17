49c660f56aac5961dc27f62b91733f6cc2b6baa6
Shoptorshi Jamil <sjami002@gmail.com>
2019-02-17 19:42:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function theBeatlesPlay(musicians, instruments) {
 
   }return empty;
 }
+
+function johnLennonFacts(facts) {
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+  ];
+}
